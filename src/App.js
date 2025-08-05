c9a4f83716a22cb58466a7df0cfaf7c3872eb423
Kassim Amir <amir.kassim946@gmail.com>
2025-08-05 09:02:15 +0300

    commit these files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="div-container">
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+        <button className="num-button">{props.value}</button>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
